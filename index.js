92de0e4e93ffb93aa9b3b92d0a35a5b68d72e063
Anastasia Solovei <av.ignatyuk@gmail.com>
2021-06-06 19:24:44 +0300

    reloates refs into class CountdownTimer, passes arguments to the CountdownTimer as an object

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
-const refs = {
-  timer01: document.querySelector("#timer-1"),
-  fieldDays: document.querySelector('#timer-1 .field span[data-value="days"]'),
-  fieldHours: document.querySelector(
-    '#timer-1 .field span[data-value="hours"]'
-  ),
-  fieldMins: document.querySelector('#timer-1 .field span[data-value="mins"]'),
-  fieldSecs: document.querySelector('#timer-1 .field span[data-value="secs"]'),
-};
-
 class CountdownTimer {
-  constructor(selector, targetDate) {
-    this.intervalId = null;
+  constructor({ selector, targetDate }) {
+    this.interval = null;
     this.selector = selector;
     this.targetDate = targetDate;
     this.start();
   }
 
+  getRefs() {
+    return {
+      days: document.querySelector(`${this.selector} [data-value="days"]`),
+      hours: document.querySelector(`${this.selector} [data-value="hours"]`),
+      mins: document.querySelector(`${this.selector} [data-value="mins"]`),
+      sec: document.querySelector(`${this.selector} [data-value="secs"]`),
+    };
+  }
+
   start() {
-    this.intervalId = setInterval(() => {
+    this.interval = setInterval(() => {
+      const { days, hours, mins, sec } = this.getRefs();
       const currentDate = Date.now();
       const time = this.targetDate - currentDate;
 
@@ -25,18 +25,19 @@ class CountdownTimer {
         return String(value).padStart(2, "0");
       }
 
-      refs.fieldDays.textContent = pad(
-        Math.floor(time / (1000 * 60 * 60 * 24))
-      );
-      refs.fieldHours.textContent = pad(
+      days.textContent = pad(Math.floor(time / (1000 * 60 * 60 * 24)));
+      hours.textContent = pad(
         Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
       );
-      refs.fieldMins.textContent = pad(
+      mins.textContent = pad(
         Math.floor((time % (1000 * 60 * 60)) / (1000 * 60))
       );
-      refs.fieldSecs.textContent = pad(Math.floor((time % (1000 * 60)) / 1000));
+      sec.textContent = pad(Math.floor((time % (1000 * 60)) / 1000));
     }, 1000);
   }
 }
 
-const timer01 = new CountdownTimer("#timer-1", new Date("Jul 17, 2021"));
+const timer1 = new CountdownTimer({
+  selector: "#timer-1",
+  targetDate: new Date("Jul 17, 2021"),
+});
